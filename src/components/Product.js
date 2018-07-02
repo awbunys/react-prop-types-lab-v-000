1f53a6895dfbf5ff48d18ad81d42826c17fc11b2
Andrew Bunys <awbunys@gmail.com>
2018-07-02 00:10:30 +0000

    automatically backed up by learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,14 +1,35 @@
-var React = require('react')
-var ReacDOM = require('react-dom')
+import React from 'react';
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types';
 
-export class Product extends React.Component {
+export default class Product extends React.Component {
   render(){
     return (
-
+      <div>
+      {this.props.name}
+      {this.props.producer}
+      {this.props.hasWatermark}
+      {this.props.color}
+      {this.props.weight}
+      </div>
     )
   }
 }
 
 Product.propTypes = {
-  name: PropType.string.isRequired
+  name: PropTypes.string.isRequired,
+  producer: PropTypes.string,
+  hasWatermark: PropTypes.bool,
+  color: PropTypes.string,
+  weight: function(props, weight, Product) {
+    if(weight > 80 && weight < 300) {
+      return true
+    }
+    return false
+  }
+
+}
+
+Product.defaultProps = {
+  hasWatermark: false
 }
